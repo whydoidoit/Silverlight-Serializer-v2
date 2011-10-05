25a5e57df2a71c8f6ebe911763ce12e50236e2dc
Jake Ginnivan <jake.ginnivan@readify.net>
2011-10-05 13:07:56 +0800

    Restructured project Added .net and WP7 project Added Build.cmd to build all 3 projects, and create a NuGet package

---
+++ Serialization.SL/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Serialization")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mike Talbot")]
+[assembly: AssemblyProduct("Serialization")]
+[assembly: AssemblyCopyright("Copyright © Mike Talbot 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9d6447d4-d4ee-4b76-b2fc-5acad7225a44")]
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
