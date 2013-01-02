4d8f3a4759e09ca4a59890356d42cbafd9a2fd24
Evgeny Rokhlin <evgeny.rokhlin@gmail.com>
2013-01-02 14:15:28 +1100

    Fixed deserializing of Arrays of Enum values
    Found a bug when attempting to deserialize an array of Enum values.
    Added a unit test project, added a failing unit test
    Serializes_Deserializes_Array_Of_EnumValues, added a fix, verified that
    the unit test passes.

---
+++ Serialization.UnitTests/SerializerUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Serialization.UnitTests
+{
+    public enum Test
+    {
+        Zero,
+        One
+    }
+
+    [TestClass]
+    public class SerializerUnitTests
+    {
+        [TestMethod]
+        public void Serializes_Deserializes_Array_Of_EnumValues()
+        {
+            //Arrange
+            Test[] testArray = new Test[] { Test.One, Test.Zero };
+
+            //Act
+            byte[] serializedTest = SilverlightSerializer.Serialize(testArray);
+            Test[] deserializedArray = (Test[])SilverlightSerializer.Deserialize(serializedTest);
+
+            //Assert
+            CollectionAssert.AreEqual(testArray, deserializedArray);
+        }
+    }
+}
